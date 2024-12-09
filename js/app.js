a041d2224889f69c6c397d1407389e409579aff5
Samantao93 <samanta.ortuno@gmail.com>
2024-12-09 18:47:48 +0100

    developed with prototypes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,57 @@
 resultado=document.querySelector('#resultado');
 
 function Seguro(marca,year,tipo){ 
-    this.marca = marca.value;
-    this.year = year.value;
-    this.tipo = tipo.value;
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
 }
 
-const ui = new UI();
-function UI(){    
-    const btnSubmit = document.querySelector('button[type="submit"]');
+Seguro.prototype.calculoSeguro = function() {
+    const cantidadBase=3000;
+    let cantidad=0;
+    let marcaReal='';
+
+    switch(this.marca){  
+        case "1":   
+            cantidad = cantidadBase * 1.15;
+            marcaReal = 'Americano';
+            break;
+        case "2":  
+            cantidad = cantidadBase * 1.05;
+            marcaReal = 'Asiático';
+            break;
+        case "3":  
+            cantidad = cantidadBase * 1.25;
+            marcaReal = 'Europeo';
+            break;
+        default:
+            break;
+    }
+
+    let tipoReal='';
+    switch(this.tipo){
+        case 'basico':
+            cantidad *= 1.05;
+            tipoReal='Básico'
+            break;
+        case 'completo':
+            cantidad *= 1.15;
+            tipoReal='Completo'
+            break;
+        default:
+            break;
+    }
+
+    const yearNow = new Date().getFullYear();
+
+    cantidad-=(yearNow-this.year)*20
+
+    return [cantidad,marcaReal,tipoReal];
+}
+
+function UI(){}
+
+UI.prototype.seleccionYear = function() {
     const year=document.querySelector('#year');
 
     const yearNow = new Date().getFullYear();
@@ -23,23 +66,54 @@ function UI(){
         }        
         year.appendChild(option);        
     }
-    
-    btnSubmit.addEventListener('click',listener);
+}
 
+UI.prototype.mostrarResultado = function(seguro,total,marcaReal) {
+    const res = document.createElement('DIV');
+    res.classList.add('mt-10','invento');
+    
+    res.innerHTML=`
+    <p class='header'>CÁLCULO DEL SEGURO</p>
+    <p class='font-bold'>Marca: <span class='font-normal'> ${marcaReal}</span></p>
+    <p class='font-bold'>Año: <span class='font-normal'>${seguro.year}</span></p>
+    <p class='font-bold'>Tipo: <span class='font-normal capitalize'>${seguro.tipo}</span></p>
+    <p class='font-bold'>Total: <span class='font-normal'>${Math.round(total)} €</span></p>
+    `    
+    resultado.appendChild(res);
+}
+
+UI.prototype.eliminarError = function() {
+    if(document.querySelector('.invento')){
+        document.querySelector('.invento').remove();
+    }
+}
+
+const ui = new UI();
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.seleccionYear();
+})
+
+eventListeners();
+function eventListeners(){
+    const btnSubmit = document.querySelector('button[type="submit"]');
+    btnSubmit.addEventListener('click',validacion);
 }
 
 
-function listener(e){
+function validacion(e){
     e.preventDefault();
-    const marca=document.querySelector('#marca');
-    const year=document.querySelector('#year');
-    const tipo=document.querySelector('input[name="tipo"]:checked');
+    const marca=document.querySelector('#marca').value;
+    const year=document.querySelector('#year').value;
+    const tipo=document.querySelector('input[name="tipo"]:checked').value;    
     
     const cargando = document.querySelector('#cargando');
+
+    ui.eliminarError();
     
-    if(marca.value==='seleccionar' || year.value==='seleccionar'){
+    if(marca==='seleccionar' || year==='seleccionar'){
         const mensajeError=document.createElement('P');
-        mensajeError.classList.add('error');
+        mensajeError.classList.add('mensaje','error','mt-10');
         mensajeError.textContent='TODOS LOS CAMPOS SON OBLIGATORIOS, REVISE LA SELECCIÓN';
         resultado.appendChild(mensajeError);
 
@@ -52,73 +126,18 @@ function listener(e){
 
     cargando.style.display='block';
 
-    if(document.querySelector('.correcto')){
-        document.querySelector('.correcto').remove();
-    }
-
     setTimeout (() => {
         cargando.style.display='none';
         const seguro = new Seguro(marca,year,tipo);
-        calculoSeguro(seguro);
+        const total = seguro.calculoSeguro()[0];   
+        const marcaReal = seguro.calculoSeguro()[1];      
+
+        ui.mostrarResultado(seguro,total,marcaReal);
     },3000)
 
 }
 
-function calculoSeguro(seguro) {
-    const {marca,year,tipo} = seguro;
-    let cantidadBase=3000;
-    let cantidad=0;
-    let marcaReal='';
 
-    switch(marca){  
-        case "1":   
-            cantidad = cantidadBase * 1.15;
-            marcaReal = 'Americano';
-            console.log(`Americano: ${cantidad}`);
-            break;
-        case "2":  
-            cantidad = cantidadBase * 1.05;
-            marcaReal = 'Asiático';
-            console.log(`Asiático: ${cantidad}`);
-            break;
-        case "3":  
-            cantidad = cantidadBase * 1.25;
-            marcaReal = 'Europeo';
-            console.log(`Europeo: ${cantidad}`);
-            break;
-        default:
-            break;
-    }
-
-    switch(tipo){
-        case 'basico':
-            cantidad *= 1.05;
-            console.log(`Basico: ${cantidad}`);
-            break;
-        case 'completo':
-            cantidad *= 1.15;
-            console.log(`Completo: ${cantidad}`);
-            break;
-        default:
-            break;
-    }
-
-    const yearNow = new Date().getFullYear();
-
-    cantidad=cantidad-(yearNow-year)*20
-
-    const res = document.createElement('DIV');
-    res.classList.add('correcto');
-    res.innerHTML=`
-        RESULTADO DEL SEGURO
-
-        Marca: ${marcaReal};
-        Año: ${year};
-        Tipo: ${tipo};
-        Cantidad: ${Math.round(cantidad)}
-    `    
-    resultado.appendChild(res);
-}
 
 /* ui; */
 /* >
